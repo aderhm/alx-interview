4f136d84313bba02b04271e7dc74dbaf47978eff
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-08-20 16:16:06 +0100

    Make change - fewest number of coins

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Change comes from within.
+"""
+
+
+def makeChange(coins, total):
+    """ Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+    """
+    if total <= 0:
+        return 0
+    noc = 0
+    for i in sorted(coins, reverse=True):
+        q = total / i
+        noc += q
+        total %= i
+
+    if total > 0:
+        return -1
+    return int(noc)
